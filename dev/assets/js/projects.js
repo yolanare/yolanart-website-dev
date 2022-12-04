f13ddda70a12e922350b808081f0fad34d046974
Yolanare <y09a3115@gmail.com>
2022-12-04 17:30:58 +0100

    added projects (unfinished) & fixes

--- dev/assets/js/projects.js
+++ dev/assets/js/projects.js
@@ -48,20 +48,20 @@ const projectsDataSample = {
         titleShort : "SHORT_TITLE",
         titleHide: false|true,
         date : "YYYY.MM",
-        category : "artworks|render|motion|brandings|posters|web|other",
+        category : "artworks|renders|motion|brandings|prints|web|other",
         context: "personal|fun|order|school|sc|ppm|retrosaturn|pti",
-        filter: "experiment|photo|illustration|3D|motion|type|poster|logo|vector|layout|ad",
+        filter: "experiment|photo|illustration|3D|motion|type|poster|print|logo|vector|layout|ad",
         subtitle : "QUICK_CATCHPHRASE",
         desc : {
             fr : `
-                FRENCH DESCRIPTION
+                FRENCH_DESCRIPTION
             `,
             en : `
-                ENGLISH DESCRIPTION
+                ENGLISH_DESCRIPTION
             `
         }
     },
-    "COMMENTS" : {
+    Comments : {
         type : "img|yt|g_pdf|embed",
                 ext : "png", // IF TYPE: "img" : if anything other than "jpg" file | DEFAULT: "jpg"
                 needBG : false|true|"000000"|"var(--y-b1)", // useful for monochrome white logos | you can specify the color of choice, CAN BE CSS variable | "true" is default dark | DEFAULT: false
@@ -73,16 +73,16 @@ const projectsDataSample = {
         titleShort : "SHORT_TITLE", // to use if title is too long to fit the project card
         titleHide: false|true, // choose whether to hide or no the title | DEFAULT: "false"
         date : "YYYY.MM", // CAN BE: "2021-2022" | "2020.09-11" | "2019-2021.03" | "2019.07-2020.02"
-        category : "artworks|render|motion|brandings|posters|web|other", // CAN BE: "artworks" | "brandings|web"]
+        category : "artworks|renders|motion|brandings|prints|web|other", // CAN BE: "artworks" | "brandings|web"]
         context: "personal|fun|order|school|retrosaturn|pti|sc", // CAN BE: "fun" | "fun|school"]
-        filter: "experiment|photo|illustration|3D|motion|type|poster|logo|vector|layout|ad", // CAN BE: "experiment" | "experiment|motion"]
+        filter: "experiment|photo|illustration|3D|motion|type|poster|print|logo|vector|layout|ad", // CAN BE: "experiment" | "experiment|motion"]
         subtitle : "QUICK_CATCHPHRASE", // CAN BE: empty
         desc : { // CAN BE: empty
             fr : `
-                FRENCH DESCRIPTION
+                FRENCH_DESCRIPTION
             `,
             en : `
-                ENGLISH DESCRIPTION
+                ENGLISH_DESCRIPTION
             `
         }
     },
@@ -687,9 +687,9 @@ var projectsData = {
         title : "CONFÉRENCE HAUT POTENTIEL INTELLECTUEL",
         titleShort : "CONFÉRENCE H.P.I.",
         date : "2019.11",
-        category : "posters",
+        category : "prints",
         context: "order",
-        filter: "poster|vector|layout|ad"
+        filter: "poster|print|vector|layout|ad"
     },
     "pdf_apc_wvhp" : {
         type : "g_pdf", interact : "off",
@@ -697,9 +697,9 @@ var projectsData = {
         title : "FORMATION WISC-V & HAUT POTENTIEL",
         titleShort : "FORMATION WISC-V & H.P.",
         date : "2019.08",
-        category : "posters",
+        category : "prints",
         context: "order",
-        filter: "poster|vector|layout|ad"
+        filter: "poster|print|vector|layout|ad"
     },
 
 // WEBSITES
@@ -723,50 +723,306 @@ var projectsData = {
                 <p>This website is one of the very first I have created.</p>
             `,
         }
+    },
+
+// new
+
+    "broch_alien" : {
+        type : "g_pdf", interact : "off",
+            url_id : "URL_ID",
+        title : "BROCHURE ALIEN",
+        date : "YYYY.MM",
+        category : "prints",
+        context: "school",
+        filter: "experiment|print|layout|ad",
+        desc : {
+            fr : ``,
+            en : `
+                school project context, not official
+            `
+        }
+    },
+    "chara_nain_de_jardin" : {
+        type : "img", interact : "zoom",
+        title : "NAIN DE JARDIN - Dum Grimnir",
+        titleShort : "NAIN DE JARDIN",
+        date : "YYYY.MM",
+        category : "artworks|other",
+        context: "school",
+        filter: "experiment|illustration",
+        desc : {
+            fr : ``,
+            en : `
+                signification nom mythologie naine
+                premier chara design/dessin propre
+            `
+        }
+    },
+    "charte_lodge_tourtereaux" : {
+        type : "g_pdf", interact : "off",
+            url_id : "URL_ID",
+        title : "CHARTE GRAPHIQUE LOGO LODGE TOURTEREAUX",
+        titleShort : "CHARTE GRPHQ. LOGO LODGE",
+        date : "YYYY.MM",
+        category : "brandings|prints",
+        context: "school",
+        filter: "experiment|type|print|logo|vector|layout",
+        desc : {
+            fr : ``,
+            en : `
+                school project context, not official
+            `
+        }
+    },
+    "abstract_heart" : {
+        type : "yt", interact : "off",
+            url_id : "URL_ID",
+        title : "ABSTRACT 3D HEART",
+        date : "YYYY.MM",
+        category : "renders",
+        context: "school",
+        filter: "experiment|3D",
+        desc : {
+            fr : ``,
+            en : `
+                normal one turnaround
+                + incrustation
+            `
+        }
+    },
+    "abstract_heart_vid" : {
+        type : "yt", interact : "off",
+            url_id : "URL_ID",
+        title : "ABSTRACT 3D HEART MAKING OFF",
+        titleShort : "ABSTRACT HEART MAKING OFF",
+        date : "YYYY.MM",
+        category : "motion",
+        context: "school",
+        filter: "3D|motion|ad",
+        desc : {
+            fr : ``,
+            en : `
+                devoir : compiler les étapes du projet en quelques minutes
+            `
+        }
+    },
+    "csg_ad" : {
+        type : "yt", interact : "off",
+            url_id : "URL_ID",
+        title : "CHACUN SES GOÛTS - PUBLICITÉ YOGURT GLACÉ",
+        titleShort : "PUB: CHACUN SES GOÛTS",
+        date : "YYYY.MM",
+        category : "renders|motion",
+        context: "school",
+        filter: "3D|motion|ad",
+        desc : {
+            fr : ``,
+            en : `
+                school project context, not official
+                + analyse
+                reporter en gros l'oral de présentation
+            `
+        }
+    },
+    "jean_lechad" : {
+        type : "img", interact : "zoom",
+        title : "Jean Lechad",
+        date : "YYYY.MM",
+        category : "artworks|other",
+        context: "personal|fun|school",
+        filter: "experiment|poster|print",
+        desc : {
+            fr : ``,
+            en : `
+                parodie
+                pour devoir scolaire
+            `
+        }
+    },
+    "logo_pti_eye" : {
+        type : "img", interact : "zoom",
+                ext : "png",
+        title : "PSYCHOID: THE INDUSTRIAX LOGO \"EYE\"",
+        titleShort : "PTI. LOGO \"EYE\"",
+        titleHide: true,
+        date : "YYYY.MM",
+        category : "brandings",
+        context: "pti",
+        filter: "experiment|type|logo|vector",
+        desc : {
+            fr : ``,
+            en : `
+                aspect losange
+                retouche un peu du sigle
+            `
+        }
+    },
+    "lonely_daft" : {
+        type : "img|yt|g_pdf|embed", interact : "zoom|off",
+                ext : "png",
+                needBG : false|"000000"|"var(--y-b1)",
+            url_id : "URL_ID",
+            embed : "FULL_URL",
+            format : "fill|16:9|1:1",
+        title : "LONELY DAFT",
+        date : "YYYY.MM",
+        category : "artworks",
+        context: "personal|retrosaturn",
+        filter: "experiment|photo",
+        desc : {
+            fr : ``,
+            en : ``
+        }
+    },
+    "gumitea_can" : {
+        type : "img", interact : "zoom",
+        title : "GUMITEA CAN",
+        date : "YYYY.MM",
+        category : "brandings|prints",
+        context: "retrosaturn",
+        filter: "illustration|3D|type|print|logo|vector|layout|ad",
+        desc : {
+            fr : ``,
+            en : `
+                parodie Tropico
+            `
+        }
+    },
+    "concrete_gnome" : {
+        type : "img", interact : "zoom",
+        title : "REINFORCED CONCRETE GNOME",
+        date : "YYYY.MM",
+        category : "artworks|prints|other",
+        context: "retrosaturn",
+        filter: "illustration|poster|print|vector|layout|ad",
+        desc : {
+            fr : ``,
+            en : `
+                no joke its important for the story
+            `
+        }
+    },
+    "y_logo_evolution" : {
+        type : "img", interact : "zoom",
+        title : "Y LOGO EVOLUTION",
+        date : "YYYY.MM",
+        category : "brandings",
+        context: "personal",
+        filter: "experiment|logo|vector",
+        desc : {
+            fr : ``,
+            en : ``
+        }
+    },
+    "zelda_badge" : {
+        type : "yt", interact : "off",
+            url_id : "URL_ID",
+            format : "16:9",
+        title : "LINK TROUVE UN BADGE D'ACCÈS",
+        titleShort : "LINK TROUVE UN BADGE",
+        date : "YYYY.MM",
+        category : "motion|other",
+        context: "personal|fun",
+        filter: "experiment|3D|motion",
+        desc : {
+            fr : ``,
+            en : `
+                expliquer en deux lignes le contexte
+                faire le making off
+            `
+        }
     }
 };
 
 const categoriesDataSample = {
     default : {
-        title: "<category>",
-        subtitle: "",
+        title: {
+            fr : "<catégorie>",
+            en : "<category>"
+        },
+        subtitle: {
+            fr : "",
+            en : ""
+        },
         icon : '<svg viewBox="0 0 32 32"><path d="M15.3,23.4l-5,3.5c-0.9,0.6-2.2-0.2-1.8-1.3l1.8-5.8c0.1-0.5,0-1-0.4-1.3L5,14.7c-0.9-0.7-0.4-2.1,0.7-2.2l6.1-0.1c0.5,0,0.9-0.3,1.1-0.8l2-5.7c0.4-1.1,1.9-1.1,2.3,0l2,5.7c0.2,0.5,0.6,0.8,1.1,0.8l6.1,0.1c1.1,0,1.6,1.5,0.7,2.2l-4.8,3.7c-0.4,0.3-0.6,0.8-0.4,1.3l1.8,5.8c0.3,1.1-0.9,2-1.8,1.3l-5-3.5C16.3,23.1,15.7,23.1,15.3,23.4z"/></svg>',
     },
 }
 const categoriesData = {
     "artworks" : {
-        title: "ARTWORKS",
-        subtitle: "Infography works",
+        title: {
+            fr : "Projets Graphique",
+            en : "Artworks"
+        },
+        subtitle: {
+            fr : "Travaux infographiques",
+            en : "Infography works"
+        },
         icon : '<svg viewBox="0 0 32 32"><path d="M21.5,7.3c1.8,0,3.2,1.5,3.2,3.2s-1.5,3.2-3.2,3.2s-3.2-1.5-3.2-3.2S19.7,7.3,21.5,7.3 M11.6,12l2.9,8.5c0,0,3.6-4.1,3.6-4.1l4.8,8.8c-1.9,1.4-4.3,2.3-6.9,2.3c-5.8,0-10.7-4.3-11.4-10C4.6,17.5,11.6,12,11.6,12 M17.9,6.7c0.7-0.6,1.6-1.1,2.6-1.3l0,0c-1.4-0.6-2.9-0.9-4.5-0.9l0,0C10,4.5,5,9.1,4.5,15l2.4-1.9c1.2-3.8,4.8-6.6,9.1-6.6C16.7,6.5,17.3,6.6,17.9,6.7L17.9,6.7z M25.3,14.1c0.1,0.6,0.2,1.2,0.2,1.9c0,2.2-0.8,4.3-2.1,6l1,1.9c1.9-2.1,3.1-4.8,3.1-7.9c0-1.6-0.3-3.1-0.9-4.5l0,0C26.4,12.5,26,13.4,25.3,14.1L25.3,14.1z"/></svg>',
     },
     "renders" : {
-        title: "3D RENDERS",
-        subtitle: "3 dimensional pixels",
+        title: {
+            fr : "Rendus 3D",
+            en : "3D Renders"
+        },
+        subtitle: {
+            fr : "Pixels en 3 dimensions",
+            en : "3 dimensional pixels"
+        },
         icon : '<svg viewBox="0 0 32 32"><path d="M26.2,15.6c2.9,1.1,4.9,2.7,4.9,4.8c0,4.1-7.8,6.3-15.1,6.3c-7.3,0-15.1-2.2-15.1-6.3c0-2.1,2-3.7,4.9-4.8l0,2.5c-1.7,0.7-2.6,1.6-2.6,2.3c0,1.6,5,4,12.8,4c7.8,0,12.8-2.4,12.8-4c0-0.7-0.9-1.6-2.6-2.3L26.2,15.6z M17.2,15.1v6.4l6.7-3.9v-6.4L17.2,15.1z M8.1,17.6l6.7,3.9v-6.4l-6.7-3.9V17.6z M9.3,9.2l6.7,3.9l6.7-3.9L16,5.3L9.3,9.2z"/></svg>',
     },
     "motion" : {
-        title: "MOTION DESIGN",
-        subtitle: "Animated pixels",
+        title: {
+            fr : "Motion-Design",
+            en : "Motion-Design"
+        },
+        subtitle: {
+            fr : "Pixels animés",
+            en : "Animated pixels"
+        },
         icon : '<svg viewBox="0 0 32 32"><path d="M2.1,20.4c1.5,2.6,4.4,4.4,7.6,4.4c1.1,0,2.1-0.2,3.1-0.6c-1-1.1-1.8-2.4-2.3-3.8L2.1,20.4z M10.5,11.6c0.5-1.4,1.3-2.7,2.3-3.8c-1-0.4-2-0.6-3.1-0.6c-3.3,0-6.1,1.8-7.6,4.4L10.5,11.6z M9.9,17.9H2.8c-1,0-1.9-0.8-1.9-1.9v0c0-1,0.8-1.9,1.9-1.9h7.1c1,0,1.9,0.8,1.9,1.9v0C11.7,17,10.9,17.9,9.9,17.9z M31.1,16c0,4.9-4,8.8-8.8,8.8s-8.8-4-8.8-8.8s4-8.8,8.8-8.8S31.1,11.1,31.1,16z M19.6,20.4l7.7-4.4l-7.7-4.4V20.4z"/></svg>',
     },
     "brandings" : {
-        title: "BRANDINGS",
-        subtitle: "Logos, banners",
+        title: {
+            fr : "Brandings",
+            en : "Brandings"
+        },
+        subtitle: {
+            fr : "Identité visuelles",
+            en : "Visual identities"
+        },
         icon : '<svg viewBox="0 0 32 32"><path d="M15.3,23.4l-5,3.5c-0.9,0.6-2.2-0.2-1.8-1.3l1.8-5.8c0.1-0.5,0-1-0.4-1.3L5,14.7c-0.9-0.7-0.4-2.1,0.7-2.2l6.1-0.1c0.5,0,0.9-0.3,1.1-0.8l2-5.7c0.4-1.1,1.9-1.1,2.3,0l2,5.7c0.2,0.5,0.6,0.8,1.1,0.8l6.1,0.1c1.1,0,1.6,1.5,0.7,2.2l-4.8,3.7c-0.4,0.3-0.6,0.8-0.4,1.3l1.8,5.8c0.3,1.1-0.9,2-1.8,1.3l-5-3.5C16.3,23.1,15.7,23.1,15.3,23.4z"/></svg>',
     },
-    "posters" : {
-        title: "POSTERS",
-        subtitle: "Posters, sometimes printed",
+    "prints" : {
+        title: {
+            fr : "Prints",
+            en : "Prints"
+        },
+        subtitle: {
+            fr : "Imprimables",
+            en : "Printed designs"
+        },
         icon : '<svg viewBox="0 0 32 32"><path d="M6.2,23L6,25.6c0,1,0.8,1.9,1.9,1.9H21c-0.3-0.5-0.5-1.2-0.5-1.9l0.1-2.6L6.2,23z M24,27.5c1,0,1.9-0.8,1.9-1.9V8.5l0.2-4H9.7l-0.1,4l0,12.9l12.7,0l-0.1,4.2C22.1,26.7,22.9,27.5,24,27.5z M21.4,17.6H14v-1.8h7.4V17.6z M21.4,13.9H14v-1.8h7.4V13.9zM21.4,10.2H14V8.3h7.4V10.2z"/></svg>',
     },
     "web" : {
-        title: "WEBSITES",
-        subtitle: "Webdesign, layouts",
+        title: {
+            fr : "Webdesign",
+            en : "Webdesign"
+        },
+        subtitle: {
+            fr : "Webdesign, layouts",
+            en : "Webdesign, layouts"
+        },
         icon : '<svg viewBox="0 0 32 32"><path d="M16,6.5c5.2,0,9.5,4.3,9.5,9.5s-4.3,9.5-9.5,9.5S6.5,21.2,6.5,16S10.8,6.5,16,6.5 M16,4.5C9.6,4.5,4.5,9.6,4.5,16S9.6,27.5,16,27.5S27.5,22.4,27.5,16S22.4,4.5,16,4.5L16,4.5z"/><path d="M16,6.5c0.9,0,3.1,3.3,3.1,9.5s-2.2,9.5-3.1,9.5c-0.9,0-3.1-3.3-3.1-9.5S15.1,6.5,16,6.5 M16,4.5c-2.8,0-5.1,5.1-5.1,11.5s2.3,11.5,5.1,11.5s5.1-5.1,5.1-11.5S18.8,4.5,16,4.5L16,4.5z"/><path d="M16,12.9c6.2,0,9.5,2.2,9.5,3.1c0,0.9-3.3,3.1-9.5,3.1S6.5,16.9,6.5,16C6.5,15.1,9.8,12.9,16,12.9 M16,10.9c-6.4,0-11.5,2.3-11.5,5.1s5.1,5.1,11.5,5.1s11.5-2.3,11.5-5.1S22.4,10.9,16,10.9L16,10.9z"/></svg>',
     },
     "other" : {
-        title: "OTHER FUNKY STUFF",
-        subtitle: "Things I do for fun",
+        title: {
+            fr : "Autres Trucs Chelous",
+            en : "Other Funky Stuff"
+        },
+        subtitle: {
+            fr : "Pour le fun",
+            en : "For fun"
+        },
         icon : '<svg viewBox="0 0 32 32"><path d="M22.4,7.9l1.3,2.3L20,12.4l-9.5,2.5L22.4,7.9 M23,5.6L4.5,16.5L5,17.7l20.8-6.3l0.2-0.6L23,5.6L23,5.6z"/><path d="M24.8,12.8l0.7,2.5l-4.4,1.2l-10.2,0L24.8,12.8 M26,10.8L4.5,16.5L5,17.7l21.4-0.7l1.2-0.5L26,10.8L26,10.8z"/><path d="M27.5,26.4h-23v-9.8h23V26.4z M22.6,23.8H25v-4.8h-2.3V23.8z M18.3,23.8h2.3v-4.8h-2.3V23.8zM13.9,23.8h2.3v-4.8h-2.3V23.8z M9,19.3c-1.2,0-2.1,1-2.1,2.1c0,1.2,1,2.1,2.1,2.1s2.1-1,2.1-2.1C11.2,20.3,10.2,19.3,9,19.3z"/></svg>'
     }
 }
@@ -828,6 +1084,7 @@ const filters = {
             "motion" : "Motion-Design",
             "type" : "Typography",
             "poster" : "Poster",
+            "print" : "Prints",
             "logo" : "Logo",
             "vector" : "Vector",
             "layout" : "Layout",
@@ -841,6 +1098,7 @@ const filters = {
             "motion" : "Motion-Design",
             "type" : "Typography",
             "poster" : "Posters",
+            "print" : "Prints",
             "logo" : "Logos",
             "vector" : "Vector",
             "layout" : "Layout",
@@ -856,6 +1114,7 @@ const filters = {
             "motion" : "Motion-Design",
             "type" : "Typographie",
             "poster" : "Affiche",
+            "print" : "Print",
             "logo" : "Logo",
             "vector" : "Vectoriel",
             "layout" : "Mise en forme",
@@ -869,6 +1128,7 @@ const filters = {
             "motion" : "Motion-Design",
             "type" : "Typographie",
             "poster" : "Affiches",
+            "print" : "Prints",
             "logo" : "Logos",
             "vector" : "Vectoriel",
             "layout" : "Mises en forme",
